95dd329578e75d949ec3feb996eed50d570083e7
Kyle <reimers95@gmail.com>
2022-12-14 19:52:59 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function addJournal (e) {
+    e.preventDefault()
+    var entry = document.querySelector("#entry")
+    var entries = document.querySelector("#entries")
+    entries.innerHTML += `<p>${entry.value}</p>`
+}
+
+
+console.log("this is from the js file!")
